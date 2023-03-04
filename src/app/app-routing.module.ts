65d22879d4078dcec0d6fa883791cf6aa7583f78
Prince Sahu <princesahu169@gmail.com>
2023-03-04 19:56:05 +0530

    Added DashBoard Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { OurMissionComponent } from './our-mission/our-mission.component';
 import { JoinUsComponent } from './join-us/join-us.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+
 const routes: Routes = [
   {path:'', component:HomepageComponent, },
   {path:'homepage', component:HomepageComponent,},
   {path:'about-us', pathMatch:'full', component:AboutUsComponent, },
   {path:'our-mission', pathMatch:'full', component:OurMissionComponent, },
   {path:'join-us', pathMatch:'full', component:JoinUsComponent, },
+  {path:'user-dashboard', pathMatch:'full', component:UserDashboardComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { OurMissionComponent } from './our-mission/our-mission.component';
 import { JoinUsComponent } from './join-us/join-us.component';
 import { ForgetPassComponent } from './forget-pass/forget-pass.component';
-
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserProfileComponent } from './user-dashboard/user-profile/user-profile.component';
+import { HelpSupportComponent } from './user-dashboard/help-support/help-support.component';
+import { LogOutComponent } from './user-dashboard/log-out/log-out.component';
+import { SettingComponentComponent } from './user-dashboard/setting-component/setting-component.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +26,12 @@ import { ForgetPassComponent } from './forget-pass/forget-pass.component';
     AboutUsComponent,
     OurMissionComponent,
     JoinUsComponent,
-    ForgetPassComponent
+    ForgetPassComponent,
+    UserDashboardComponent,
+    UserProfileComponent,
+    HelpSupportComponent,
+    LogOutComponent,
+    SettingComponentComponent
   ],
   imports: [
     BrowserModule,
