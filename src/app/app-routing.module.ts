196fcd70213ba2152a1e2f0522ba418d98cc7a57
Shrutigupta03 <shrutigupta70240@gmail.com>
2023-03-06 22:47:26 +0530

    logout updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,26 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { OurMissionComponent } from './our-mission/our-mission.component';
 import { JoinUsComponent } from './join-us/join-us.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { LoginComponent } from './login/login.component';
 import { MainDashboardComponent } from './user-dashboard/main-dashboard/main-dashboard.component';
 import { UserProfileComponent } from './user-dashboard/user-profile/user-profile.component';
 import { HelpSupportComponent } from './user-dashboard/help-support/help-support.component';
 import { SettingsComponent } from './user-dashboard/settings/settings.component';
-import { LogOutComponent } from './user-dashboard/log-out/log-out.component';
 
 const routes: Routes = [
   {path:'', component:HomepageComponent, },
-  {path:'homepage', component:HomepageComponent,},
+  {path:'homepage', pathMatch:'prefix', component:HomepageComponent,},
   {path:'about-us', pathMatch:'full', component:AboutUsComponent, },
   {path:'our-mission', pathMatch:'full', component:OurMissionComponent, },
   {path:'join-us', pathMatch:'full', component:JoinUsComponent, },
-  {path:'main-dashboard', pathMatch:'full', component:MainDashboardComponent},
-  {path:'user-dashboard', component:UserDashboardComponent},
-  {path:'user-profile', component:UserProfileComponent},
-  {path:'help-support', component:HelpSupportComponent},
-  {path:'settings', component:SettingsComponent},
-  {path:'log-out', component:LogOutComponent},
+  {path:'about-us', component:UserDashboardComponent, children:[
+    {path:'main-dashboard', component:MainDashboardComponent},
+    {path:'user-profile', component:UserProfileComponent},
+    {path:'help-support', component:HelpSupportComponent},
+    {path:'settings', component:SettingsComponent},
+  ]},
+  {path:'login', component:LoginComponent},
+  // {path:'/homepage', component:HomepageComponent},
 ];
 
 @NgModule({
