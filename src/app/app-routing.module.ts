de34e1f74e128b0f0089640ae9b21e6bb610c36e
Shrutigupta03 <shrutigupta70240@gmail.com>
2023-03-06 15:22:07 +0530

    dashboard nav updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { OurMissionComponent } from './our-mission/our-mission.component';
 import { JoinUsComponent } from './join-us/join-us.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { MainDashboardComponent } from './user-dashboard/main-dashboard/main-dashboard.component';
+import { UserProfileComponent } from './user-dashboard/user-profile/user-profile.component';
+import { HelpSupportComponent } from './user-dashboard/help-support/help-support.component';
+import { SettingsComponent } from './user-dashboard/settings/settings.component';
+import { LogOutComponent } from './user-dashboard/log-out/log-out.component';
 
 const routes: Routes = [
   {path:'', component:HomepageComponent, },
@@ -12,11 +17,16 @@ const routes: Routes = [
   {path:'about-us', pathMatch:'full', component:AboutUsComponent, },
   {path:'our-mission', pathMatch:'full', component:OurMissionComponent, },
   {path:'join-us', pathMatch:'full', component:JoinUsComponent, },
-  {path:'user-dashboard', pathMatch:'full', component:UserDashboardComponent},
+  {path:'main-dashboard', pathMatch:'full', component:MainDashboardComponent},
+  {path:'user-dashboard', component:UserDashboardComponent},
+  {path:'user-profile', component:UserProfileComponent},
+  {path:'help-support', component:HelpSupportComponent},
+  {path:'settings', component:SettingsComponent},
+  {path:'log-out', component:LogOutComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
